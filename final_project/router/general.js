769af70b3e2bd9c769a970c32ad79103ff14e19a
rowryNninah <rorisang.mosapa@umuzi.org>
2025-09-02 10:13:23 -0400

    added task 10-11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   let {username, password} = req.body;
@@ -21,79 +20,91 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
-  return res.status(200).send(JSON.stringify(books, null, 4));
+    try{
+        const data = await new Promise((resolve)=>{
+            resolve(books)
+        })
+        return res.status(200).json(data);
+    }catch(e){
+        return res.status(500).json({message: "An error occurred while fetching books"})
+    }
+ 
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-
   const isbn = req.params.isbn;
-  const book = books[isbn]
-
-  if(book){
-    res.status(200).send(JSON.stringify(book,null, 4))
-  }else{
-    return res.status(404).json({message: "Book not found"});
+  try{
+    const book = await new Promise((resolve, reject) =>{
+        if(books[isbn]){
+            resolve(books[isbn])
+        }else{
+            reject(new Error("Book not found"))
+        }
+    })
+    res.status(200).send(JSON.stringify(book))
+  }catch(e){
+    return res.status(404).json({message: e.message});
   }
-
-  
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
     //Write your code here
     const author = req.params.author;
-    const authorKey = Object.keys(books);
-    let results = []
+    try{
+        const results = await new Promise((resolve, reject) =>{
+            const filteredBooks = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase())
+
+            if(filteredBooks.length > 0){
+                resolve(filteredBooks)
+            }else{
+                reject(new Error("No Books found for this author"))
+            }
+        })
+        return res.send(JSON.stringify(results)); 
+
+    }catch(e){
+        res.status(404).send({ message: e.message});
+    }
   
-    authorKey.forEach((key) =>{
-        if(books[key].author.toLowerCase() === author.toLocaleLowerCase()){
-            results.push(books[key])
-        }
-    })
-    
-    if (results.length > 0) {
-        res.send(JSON.stringify(results, null, 4));  // neatly format results
-      } else {
-        res.status(404).send({ message: "No books found for this author" });
-      }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   //Write your code here
   const title = req.params.title;
-    const titleKey = Object.keys(books);
-    let results = []
-  
-    titleKey.forEach((key) =>{
-        if(books[key].title.toLowerCase() === title.toLocaleLowerCase()){
-            results.push(books[key])
-        }
-    })
-    
-    if (results.length > 0) {
-        res.send(JSON.stringify(results, null, 4)); 
-      } else {
-        res.status(404).send({ message: "No books found for this author" });
-      }
+
+    try{
+        const results = await new Promise((resolve, reject) =>{
+            const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase())
+            if(filteredBooks.length > 0){
+                resolve(filteredBooks)
+            }else{
+                reject(new Error("No Books found by this title"))
+            }
+        })
+        return res.send(JSON.stringify(results));
+
+    }catch(e){
+        return res.status(404).send({ message: e.message});
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;   
-  const book = books[isbn];      
-
+  const book = books[isbn]; 
   if (book) {
     res.send(JSON.stringify(book.reviews, null, 4)); 
   } else {
     res.status(404).send({ message: "Book not found" });
   }
-  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 module.exports.general = public_users;
